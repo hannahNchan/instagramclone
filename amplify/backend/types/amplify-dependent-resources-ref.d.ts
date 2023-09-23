a9d49cbd85bcf0516f2a1025846eb7367614ef82
hannahNchan <hannah.n.kahnwald@gmail.com>
2023-09-23 03:15:16 +0000

    minor updates

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -25,5 +25,11 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     }
+  },
+  "storage": {
+    "s3c3799950": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
